3dab0006fbd6ff800843bb1ee8a9355be53f9d44
Maksym Cherniaiev <likeran2@gmail.com>
2018-10-25 22:54:00 +0000

    Uploaded the solution

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const a = parseFloat(prompt("enter A value"));
+const b = parseFloat(prompt("enter B value"));
+const c = parseFloat(prompt("enter C value"));
+
+function quadEqSolve(a, b, c) {
+    const d = b * b - 4 * a * c;
+    if (d < 0) {
+        return "Equation Has No Solution";
+    } else if (d >= 0) {
+        const x1 = (- (b) - Math.sqrt(d)) / (2 * a);
+        const x2 = (- (b) + Math.sqrt(d)) / (2 * a);
+        return ("x1 = " + x1 + "x2 = " + x2);
+    }
+}
+
+alert(quadEqSolve(a, b, c));
